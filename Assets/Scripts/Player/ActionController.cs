507661ed74717cb9e53da1f8fa7fa493afe2621b
Semoy <89610220+Semoyy@users.noreply.github.com>
2024-01-11 18:20:39 -0500

    yeet

--- Assets/Scripts/Player/ActionController.cs
+++ Assets/Scripts/Player/ActionController.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class ActionController : MonoBehaviour
 {
     private PlayerInputs inputs;
+    [SerializeField] private AudioSource basicAttackSound;
+    [SerializeField] private AudioSource heavyAttackSound;
     [SerializeField] private AudioSource whirlwindSound;
+    [SerializeField] private AudioSource conversionSound;
 
     private void Update()
     {
         if (inputs.Action1.OnDown)
         {
+            basicAttackSound.Play();
             Debug.Log("BasicAttack");
         }
         if (inputs.Action2.OnDown)
         {
+            heavyAttackSound.Play();
             Debug.Log("HeavyStrike");
         }
         if (inputs.Action3.OnDown)
@@ -22,12 +27,24 @@ public class ActionController : MonoBehaviour
         }
         if (inputs.Action4.OnDown)
         {
+            conversionSound.Play();
             Debug.Log("Conversion");
+        }
+        if (inputs.Action1.OnUp)
+        {
+            basicAttackSound.Stop();
+        }
+        if (inputs.Action2.OnUp)
+        {
+            heavyAttackSound.Stop();
         }
         if (inputs.Action3.OnUp)
         {   
             whirlwindSound.Stop();
-            Debug.Log("Whirlwind");
+        }
+        if (inputs.Action4.OnUp)
+        {   
+            conversionSound.Stop();
         }
     }
 
