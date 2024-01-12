040100de9ee8419714c1a3d4a8086a44606a0535
GalahadIII <89609656+GalahadIII@users.noreply.github.com>
2024-01-12 08:43:31 -0500

    build setting

--- Assets/Scripts/Enemies/Modules/MovementModule.cs
+++ Assets/Scripts/Enemies/Modules/MovementModule.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 [RequireComponent(typeof(EnemyBase))]
 public class MovementModule : MonoBehaviour
